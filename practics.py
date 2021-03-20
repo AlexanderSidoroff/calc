269b98a3903f44b3c7874f547a129a39bbe90b98
Alexandr <Alxandr.Sidorov@gmail.com>
2021-03-20 11:45:56 +0300

    classwork

---
+++ practics.py
@@ -0,0 +1,24 @@
+instr = input('Введите выражение: ').replace(' ','')
+
+#Распарсить
+
+hp_ops = tuple('^')
+mp_ops = tuple('*/')
+lp_ops = tuple('+-')
+supported_ops = hp_ops + mp_ops + lp_ops
+digit_chars = tuple(map(str, range(10))) + tuple('.-')
+
+actions = list()
+d = dict()
+d['opr'] = 'First'
+d['val'] = None
+actions.append(d)
+
+
+for letter in instr:
+    if letter in supported_ops and (i > 0):
+        actions.append({'opr': letter, 'val': ''})
+        pass
+    elif letter in digit_chars:
+        actions[-1]['val'] += letter
+        pass
