2af05e2f8927016f2e10f4b21124cba7640c029f
Alexandr <Alxandr.Sidorov@gmail.com>
2021-04-06 17:42:33 +0300

    дз

---
+++ func.py
@@ -0,0 +1,32 @@
+def parse_command(command: str) -> list:
+    parsedlone = command.replace(' ', '')
+    res = list(parsedlone)
+    return res
+
+def calc(command: str) -> float:
+    precalc = ''.join(parse_command(command))
+    res = eval(precalc)
+    return res
+
+def getfromfile(file_name: str):
+    with open(file_name, 'r') as f:
+        line = f.readline()
+    return line
+    
+'''
+def writeinfile('output.txt': str):
+    with open('output.txt', 'w') as f:
+        f.writelines(command)
+'''
+
+import sys
+if len(sys.argv) <= 1:
+    command = input("Введите выражение:")
+    print(calc(command))
+else:
+    filename = sys.argv[1]
+    command = getfromfile(filename)    
+    print(calc(command))
+    with open('output.txt', 'w') as f:
+        f.writelines(str(calc(command)))
+    
